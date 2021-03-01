a6a5abfd58786239ef4e85dd2d5477c88d8825c9
Tristan Tongue <tristantongue@gmail.com>
2021-03-01 15:12:37 -0500

    fixed singleton again

--- src/weekone/SampleSingleton.java
+++ src/weekone/SampleSingleton.java
@@ -1,16 +1,50 @@
 package weekone;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+
 
 public class SampleSingleton {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static Connection conn = null;
+	
+	private static volatile SampleSingleton instance = null;
+	
+	
+	public static SampleSingleton getInstance() {
+		if (instance == null) {
+			synchronized (SampleSingleton.class) {
+				if (instance == null) {
+					instance = new SampleSingleton();
+				}
+			}
+		}
+		return instance;
 	}
-
+	
+	public static void databaseQuery(BigDecimal input) {
+		try {
+			conn = DriverManager.getConnection("url of database");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery("select id from table");
+			BigInteger x;
+			while(rs.next()) {
+				x = input.multiply(BigDecimal.valueOf(rs.getInt(1))).toBigInteger();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
